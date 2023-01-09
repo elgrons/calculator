7ad5019025c801c1302986fd9fb4d94957682129
Eliot Gronstal <eliot.lauren@gmail.com>
2023-01-09 14:25:21 -0800

    add multiply function to calculator using javascript.
    Co-Authored By: Mesha (madsketchz@gmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,13 @@ function subtract (number3, number4) {
   const number4= parseInt(prompt("Enter a fourth number:"));
 
   window.alert(subtract(number3, number4));
+
+  function multiply(number5, number6) {
+    return number5 * number6;
+  }
+
+  const number5= parseInt(prompt("Enter a fifth number:"));
+  const number6= parseInt(prompt("Enter a sixth number:"));
+
+  window.alert(multiply(number5, number6));
+  
