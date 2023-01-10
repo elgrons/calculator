27b6f3cb02ca9ce445dd46ca51f4d3342e61e119
Eliot Gronstal <eliot.lauren@gmail.com>
2023-01-09 16:26:55 -0800

    cleaned up javascript style and added phrase your sum is to line 26.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,33 @@
+
 // business logic
 function add(number1, number2) {
     return number1 + number2;
-  }
-  
-  // user interface logic 
-  const number1 = parseInt(prompt("Enter a number:"));
-  const number2 = parseInt(prompt("Enter another number:"));
-  
-  window.alert(add(number1, number2));
+}
 
 function subtract (number3, number4) {
-    return number3 - number4(prompt("Your sum is:"));
-  }
-
-  const number3= parseInt(prompt("Enter a third number:"));
-  const number4= parseInt(prompt("Enter a fourth number:"));
-
-  window.alert(subtract(number3, number4));
+  return number3 - number4;
+}
 
 function multiply(number5, number6) {
-  return number5 * number6;
-  }
-
-  const number5= parseInt(prompt("Enter a fifth number:"));
-  const number6= parseInt(prompt("Enter a sixth number:"));
-
-  window.alert(multiply(number5, number6));
+  return number5 * number6(prompt("your sum is:"));
+}
   
 function divide(number7, number8) {
   return number7 / number8;
 }
-  const number7= parseInt(prompt("Enter a seventh number"));
-  const number8= parseInt(prompt("Enter a Eighth number"));
+  // user interface logic 
+  const number1 = parseInt(prompt("Enter a number:"));
+  const number2 = parseInt(prompt("Enter another number:"));
+
+  window.alert(add(number1, number2));
+
+
+  
+  window.alert("Your sum is:" + subtract(number3, number4) + ".");
+
+
+  window.alert(multiply(number5, number6));
+
+
 
   window.alert(divide(number7, number8));
