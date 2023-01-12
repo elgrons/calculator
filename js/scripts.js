20b246ad1bf44dd99164008ae4fdb1e7c0970ba0
Eliot Gronstal <eliot.lauren@gmail.com>
2023-01-12 15:02:27 -0800

    add if/else logic.
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>
    Co-Authored: Mesha Devan <madsketchz@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -20,15 +20,30 @@ function divide(number1, number2) {
 
 function handleCalculation(event) {
   event.preventDefault();
-  const number1 = parseInt(document.querySelector("input1#input1").value);
-  const number2 = parseInt(document.querySelector("input2#input2").value);
+  const number1 = parseInt(document.querySelector("input#input1").value);
+  const number2 = parseInt(document.querySelector("input#input2").value);
   const operator = document.querySelector("input[name='operator']:checked").value;
+
+  let result;
+  if (operator === "add") {
+    result = add(number1, number2);
+  } else if (operator === "subtract") {
+    result = subtract(number1, number2);
+  } else if (operator === "multiply") {
+    result = multiply(number1, number2);
+  } else if (operator === "divide") {
+    result = divide(number1, number2);
+  } 
+
+  document.getElementById("output").innerText = result;
 } //the code to get and process form values will go here!
 
 window.addEventListener ("load", function() {
   const form = document.getElementById("calculator");
   form.addEventListener("submit", handleCalculation);
 }); //the code that makes the form work based on multiple functions!
+
+// window alert and prompt verion
 
 // const number1 = parseInt(prompt("Enter a number:"));
 // const number2 = parseInt(prompt("Enter another number:"));
