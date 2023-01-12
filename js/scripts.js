8ae23db1728438463b0b5bd9ebdc805329cced47
Eliot Gronstal <eliot.lauren@gmail.com>
2023-01-12 11:59:27 -0800

    add handleCalcuation function.
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-
 // business logic
+
 function add(number1, number2) {
   return number1 + number2;
 }
@@ -15,14 +15,28 @@ function multiply(number1, number2) {
 function divide(number1, number2) {
   return number1 / number2;
 }
-  // user interface logic 
-  const number1 = parseInt(prompt("Enter a number:"));
-  const number2 = parseInt(prompt("Enter another number:"));
 
-  window.alert(add(number1, number2));
+// user interface logic 
 
-  window.alert("Your sum is:" + subtract(number1, number2) + ".");
+function handleCalculation(event) {
+  event.preventDefault();
+  const number1 = parseInt(document.querySelector("input1#input1").value);
+  const number2 = parseInt(document.querySelector("input2#input2").value);
+  const operator = document.querySelector("input[name='operator']:checked").value;
+} //the code to get and process form values will go here!
 
-  window.alert(multiply(number1, number2));
+window.addEventListener ("load", function() {
+  const form = document.getElementById("calculator");
+  form.addEventListener("submit", handleCalculation);
+}); //the code that makes the form work based on multiple functions!
 
-  window.alert(divide(number1, number2));
+// const number1 = parseInt(prompt("Enter a number:"));
+// const number2 = parseInt(prompt("Enter another number:"));
+
+//window.alert(add(number1, number2));
+
+//window.alert("Your sum is:" + subtract(number1, number2) + ".");
+
+//window.alert(multiply(number1, number2));
+
+//window.alert(divide(number1, number2));
